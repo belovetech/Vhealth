5dae4371931cb657097799dcc6c4636ebd941a2a
Ade1fe <addypearl09@gmail.com>
2023-04-22 17:54:59 +0100

    health section

--- frontend/Assets/js/app.js
+++ frontend/Assets/js/app.js
@@ -15,3 +15,50 @@ menuToggle.addEventListener('click', () => {
         clickable: true,
       },
     });
+
+
+
+    // health condition swipperTwo
+    var illnesses = [
+    "Heart disease",
+    "Stroke",
+    "Lung cancer",
+    "Alzheimer's disease",
+    "Diabetes",
+    "Kidney disease",
+    "Liver disease",
+    "Depression",
+    "Schizophrenia",
+    "Bipolar disorder",
+    "Eating disorders",
+    "Autism spectrum disorders",
+    "Multiple sclerosis",
+    "Parkinson's disease",
+    "Huntington's disease",
+    "Amyotrophic lateral sclerosis (ALS)",
+    "HIV/AIDS",
+    "Malaria",
+    "Tuberculosis",
+    "COVID-19"
+  ];
+
+  var swiperTwoWrapper = document.querySelector(".swiperTwo .swiper-wrapper");
+
+  for (var i = 1; i < illnesses.length; i++) {
+    var slide = document.createElement("div");
+    slide.classList.add("swiper-slide");
+    slide.textContent = illnesses[i];
+    swiperTwoWrapper.appendChild(slide);
+  }
+
+  var swiperTwo = new Swiper(".swiperTwo", {
+    slidesPerView: 3,
+    grid: {
+      rows: 3,
+    },
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
