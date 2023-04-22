c0d474c8990b6ad9fbb9e34478379fbfede022f0
belovetech <belovetech@gmail.com>
2023-04-22 19:48:11 +0100

    [Update]: schedule model

--- backend/models/Schedule.js
+++ backend/models/Schedule.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 
 const ScheduleSchema = new mongoose.Schema({
   providerId: String,
+  patientId: String,
   date: String,
   time: Date,
+  status: {
+    type: String,
+    enum: {
+      values: ['availability', 'on-going', 'non-availability'],
+    },
+    default: 'availability',
+  },
 });
 
 const Schedule = mongoose.model('Schedule', ScheduleSchema);
