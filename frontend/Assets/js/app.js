f69da22457eb93f98aa6238322ce81bf099a8587
Ade1fe <addypearl09@gmail.com>
2023-04-22 16:04:10 +0100

    modified header

---
+++ frontend/Assets/js/app.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const links = document.querySelector('.links');
+
+menuToggle.addEventListener('click', () => {
+  links.classList.toggle('active');
+  console.log("hello")
+});
