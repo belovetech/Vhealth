01c55f0069afd8e6cbed4da9a2f470a320a09a72
belovetech <belovetech@gmail.com>
2023-04-25 08:45:26 +0100

    [Feat]: vhealth swagger documentation config file

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yamljs');
+
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
 const providerRouter = require('./routes/providerRouter');
@@ -13,21 +18,31 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-const corOptions = {
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-};
-app.use(cors(corOptions));
+app.use(cors());
 
 // Development logger
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use('/auth', authRouter);
-app.use('/users', userRouter);
-app.use('/providers', providerRouter);
-app.use('/appointements', appointementRouter);
+// Swagger Documentation config
+const file = fs.readFileSync(path.join(__dirname, 'swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(file);
+
+const options = {
+  explorer: true,
+  customSiteTitle: 'Vhealth',
+};
+app.use(
+  '/api/v1/vhealth/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, options)
+);
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/providers', providerRouter);
+app.use('/api/v1/appointments', appointementRouter);
 
 app.use('*', (req, res, next) =>
   res.status(404).json({ Error: 'This route was not defined on this server' })
